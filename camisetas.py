4d6b5bd4c6bfb3e9ecf77e7276b9591c9d032115
Gabriel <99749672+aieFaria@users.noreply.github.com>
2024-01-23 05:35:22 -0300

    Atualização de camisetas.py
    Com esta conseguimos 100/100 no corretor online do site OBL. Nesta versão está funcionando como pedido no exercício.

--- camisetas.py
+++ camisetas.py
@@ -1,21 +1,18 @@
-#!/data/user/0/ru.iiec.pydroid3/files/x86_64-linux-android/bin/python3
+#!/usr/bin/env python3
+#/data/user/0/ru.iiec.pydroid3/files/x86_64-linux-android/bin/python3
 
 N = input()
 nP = 0
 nM = 0
 
-for Ti in range(int(N)):
-    Ti = int(input())
-    
-    if Ti == 1:
-        nP = nP+1
-    elif Ti == 2:
-        nM = nM+1
+Ti = input().split()
+#Ti = Ti.split()
+#print(Ti)
         
 P = int(input())
 M = int(input())
 
-if nP >= P and nM >= M:
+if P >= Ti.count('1') and M >= Ti.count('2'):
     print('S')
 else:
     print('N')
