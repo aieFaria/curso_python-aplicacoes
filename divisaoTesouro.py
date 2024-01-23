2079634bb1150c4c0db1537a85fcce5a58f7e98a
Gabriel <99749672+aieFaria@users.noreply.github.com>
2024-01-23 16:37:22 -0300

    Add question OBI: Divisão do Tesouro

---
+++ divisaoTesouro.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#/data/user/0/ru.iiec.pydroid3/files/x86_64-linux-android/bin/python3
+
+A = int(input('Moedas: '))
+N = int(input('Num Marinheiros: '))
+
+
+print(2*(A/(N+2)))
